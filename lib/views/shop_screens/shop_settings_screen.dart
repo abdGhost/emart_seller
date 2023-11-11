32c3e7e7b5158636817f1e5a8681bf48c842ddfe
unknown <ktaher642@gmail.com>
2023-11-10 21:15:29 -0800

    start working on shop_setting_screen

---
+++ lib/views/shop_screens/shop_settings_screen.dart
@@ -0,0 +1,9 @@
+import '../../const/const.dart';
+
+class ShopSettingsScreen extends StatelessWidget {
+  const ShopSettingsScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
